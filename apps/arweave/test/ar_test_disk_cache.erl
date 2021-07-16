a15f48e84e36d79fc0cb6b9235ec3e3349cc4bdd
Taras Halturin <halturin@gmail.com>
2021-07-16 20:28:31 +0200

    Add comments to the disk cache test

--- apps/arweave/test/ar_test_disk_cache.erl
+++ apps/arweave/test/ar_test_disk_cache.erl
@@ -18,6 +18,8 @@ store_tx_block(Config) ->
 	CleanSize = trunc(Limit * ?DISK_CACHE_CLEAN_PERCENT_MAX/100),
 	Path = filename:join(AppConfig#config.data_dir, ?CACHE_DIR),
 	Wallet = ?config(wallet, Config),
+
+	% get current height and state of the disk cache
 	CurrentHeight = ar_node:get_height(),
 	{DiskCacheSizeOnStart, FilesOnStart} = filelib:fold_files(
 		Path,
@@ -27,21 +29,31 @@ store_tx_block(Config) ->
 				{Total + filelib:file_size(F),
 				[{filelib:last_modified(F), filelib:file_size(F), F}|Files]} end,
 		{0, []}),
+
+	% generate 10 blocks with 10 txs
 	lists:foldl(fun(N, {Taken, Files}) ->
+		% create a signed tx
 		TX = ar_test_lib:sign_tx(Wallet,
 								 #{
 								   data => crypto:strong_rand_bytes(102400),
 								   last_tx => ar_test_lib:get_tx_anchor()
 								  }),
+		% compute the tx file size
 		TXData = ar_serialize:jsonify(ar_serialize:tx_to_json_struct(TX)),
 		TXFileName = Path ++ "/tx/" ++ binary_to_list(ar_util:encode(TX#tx.id)) ++ ".json",
 		Files1 = [{calendar:local_time(), byte_size(TXData), TXFileName} | Files],
+
+		% post it to the node and mine a new block
 		ar_test_lib:post_tx(TX),
 		ar_test_lib:mine(),
+
+		% compute the block file size
 		Block = ar_test_lib:wait_for_block(N),
 		BlockData = ar_serialize:jsonify(ar_serialize:block_to_json_struct(Block)),
 		BlockFileName = Path ++ "/block/" ++ binary_to_list(ar_util:encode(Block#block.indep_hash)) ++ ".json",
 		Files2 = [{calendar:local_time(), byte_size(BlockData), BlockFileName} | Files1],
+
+		% Check how disk cache handled new comes files.
 		Size = byte_size(BlockData) + byte_size(TXData),
 		case Size + Taken of
 			NewTaken when NewTaken > Limit ->
